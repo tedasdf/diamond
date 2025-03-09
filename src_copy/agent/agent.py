0974e834f798947638522f9be74bed21b71007c4
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-03-09 19:40:13 +1100

    implement a structure that people can do differrnt algorithms and diffsuion model

---
+++ src_copy/agent/agent.py
@@ -0,0 +1,9 @@
+
+
+
+
+#this is the base class for reinforcement learning agent
+class Agent():
+    def __init__():
+        raise NotImplementedError
+    
---
+++ src_copy/env/env.py
@@ -0,0 +1,7 @@
+
+
+
+# this is the base class for environement
+class env_model():
+    def __init__():
+        raise NotImplementedError
