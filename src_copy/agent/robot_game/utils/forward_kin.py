f56bc270902bf3d12100325509dd5f90b37d4ade
tedasdf <154312357+tedasdf@users.noreply.github.com>
2025-04-22 15:09:48 +1000

    update

---
+++ src_copy/agent/robot_game/utils/forward_kin.py
@@ -0,0 +1,21 @@
+
+
+
+
+if __name__ == "__main__":
+    # forward kinematics : using the 
+
+    S = []
+    w = []
+    v = []
+    theta = []
+
+
+
+
+    for i in range(3):
+        S.append((w[i], v[i]))
+
+
+
+    
